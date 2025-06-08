7b6e3875a10b2e17af9056bb66ae018c3c056502
GCouzzi <gabrielcouzzihb20@gmail.com>
2025-06-08 17:31:25 -0300

    [Backend] Dockerfile e docker-compose.yml criados

--- backend/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147495c6f93152e92c033dc3c3448f031d590042")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
